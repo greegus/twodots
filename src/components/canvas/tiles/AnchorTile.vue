b309845d190297de7acd197e7f077a21a4f6716d
Matúš Duchoň <matus.duchon@gmail.com>
2020-04-28 09:21:44 +0200

    Fix anchor tile

--- src/components/canvas/tiles/AnchorTile.vue
+++ src/components/canvas/tiles/AnchorTile.vue
@@ -1,5 +1,21 @@
 <template>
   <g>
-    <image xlink:href="~/assets/images/tiles/anchor.svg" x=".225" y=".225" width=".55" height=".55" />
+    <image
+      class="object-contain overflow-hidden"
+      href="~/assets/images/tiles/anchor.svg"
+      x=".225"
+      y=".225"
+      width=".55"
+      height=".55"
+      preserveAspectRatio="none"
+    />
   </g>
 </template>
+
+<script>
+export default {
+  props: {
+    tile: Object
+  }
+}
+</script>
