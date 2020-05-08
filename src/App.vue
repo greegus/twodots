56cc40d3017b7ebc199364cf7fd192ac466a1a24
Matúš Duchoň <matus.duchon@gmail.com>
2020-05-08 11:38:32 +0200

    Fades between screens

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="bg-gray-100 select-none font-sans font-light">
-    <router-view class="h-screen w-screen overflow-y-scroll" />
+  <div class="w-screen h-screen overflow-hidden bg-gray-100 select-none font-sans font-light">
+    <FadeTransition :duration="300">
+      <router-view class="h-screen w-screen overflow-y-auto" />
+    </FadeTransition>
   </div>
 </template>
+
+<script>
+import FadeTransition from 'transitions/Fade'
+
+export default {
+  components: {
+    FadeTransition
+  }
+}
+</script>
 
 <style>
   @import url('./assets/styles/styles.css');
