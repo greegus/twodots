3a7dc23f6be012098d397c533cb58e8cfeb0e311
Matúš Duchoň <matus.duchon@gmail.com>
2020-05-14 11:17:32 +0200

    deploy

---
+++ precache-manifest.31bc0b37960898a5e5d9c9f52a70c77c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ccf3a7febc1161a5c6",
+    "url": "/twodots/css/app.03b05b2a.css"
+  },
+  {
+    "revision": "af5e945efabe41bf55e3feebe48cc6ba",
+    "url": "/twodots/img/anchor.af5e945e.svg"
+  },
+  {
+    "revision": "63a80717e0252cc6306a4ad8085d2de3",
+    "url": "/twodots/img/aqua.63a80717.svg"
+  },
+  {
+    "revision": "aaede6ca7b24198f090e58783402d6fc",
+    "url": "/twodots/img/ice.aaede6ca.svg"
+  },
+  {
+    "revision": "2d8786a4dcd7f24e3efe2f016afe46f2",
+    "url": "/twodots/img/ramp.2d8786a4.svg"
+  },
+  {
+    "revision": "a8931190e69838f227e8757480815db9",
+    "url": "/twodots/index.html"
+  },
+  {
+    "revision": "79ccf3a7febc1161a5c6",
+    "url": "/twodots/js/app.dbde80dd.js"
+  },
+  {
+    "revision": "0b24847eb7630f95a491",
+    "url": "/twodots/js/chunk-vendors.469863eb.js"
+  },
+  {
+    "revision": "d9bdfd2f4f080694a5f4193ed5aa8194",
+    "url": "/twodots/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/twodots/robots.txt"
+  }
+]);
